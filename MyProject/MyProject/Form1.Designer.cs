a306940a9c93d9c8529b2244e9284b693fdeb1ca
Kolugin <nikita_chizh@mail.ru>
2023-04-02 12:59:14 +0300

    Form Registration users
    Form Registration users

---
+++ MyProject/MyProject/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Регистрация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Выход из программы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(123, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Введите логин";
+            this.textBox1.Visible = false;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(123, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Введите пароль";
+            this.textBox2.Visible = false;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox3.Location = new System.Drawing.Point(123, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Повторите пароль";
+            this.textBox3.Visible = false;
+            this.textBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(123, 86);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Показать пароль";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 141);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вход в программу";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MyProject/MyProject/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        int a = 0;
+        int b = 0;
+        int c1, c2, c3 = 0;
+        string d1, d2, d3 = "";
+        int x = 0;
+        string ConS = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DBY.mdb";
+        private OleDbConnection dbCon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Выход
+        {
+            DialogResult result = MessageBox.Show("Вы действительно хотите выйти?", "Предупреждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Регистрация
+        {
+            if (a == 0)
+            {
+                c1 = 0;
+                c2 = 0;
+                c3 = 0;
+                b = 0;
+                x = 0;
+                textBox1.Visible = true;
+                textBox1.Text = "Введите логин";
+                textBox1.ForeColor = Color.Gray;
+
+                textBox2.Visible = true;
+                textBox2.UseSystemPasswordChar = false;
+                textBox2.Text = "Введите пароль";
+                textBox2.ForeColor = Color.Gray;
+
+                textBox3.Visible = true;
+                textBox3.Text = "Повторите пароль";
+                textBox3.ForeColor = Color.Gray;
+
+                checkBox1.Visible = false;
+                a = 1;
+            }
+            if (a == 1 || a>0 || a!=0)
+            {
+                if (textBox1.Text != "" && textBox1.Text != "Введите логин" || textBox2.Text != "" && textBox2.Text != "Введите пароль" || textBox3.Text != "" && textBox3.Text != "Повторите пароль")
+                {
+                    string s1 = textBox1.Text;
+                    string s2 = textBox2.Text;
+                    string q = "INSERT INTO accounts (login, pass) VALUES (@login, @pass)";
+                    if (textBox2.Text == textBox3.Text)
+                    {
+                        try
+                        {
+                            dbCon = new OleDbConnection(ConS);
+                            dbCon.Open();
+                            using (dbCon)
+                            {
+                                OleDbCommand com = new OleDbCommand(q, dbCon);
+                                com.Parameters.AddWithValue("@login", s1);
+                                com.Parameters.AddWithValue("@pass", s2);
+                                com.ExecuteNonQuery();
+                            }
+                            dbCon.Close();
+                            MessageBox.Show("Регистрация успешна!.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            a = 0;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ошибка соединения с БД!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            MessageBox.Show(Convert.ToString(a));
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пароли не свопадают, введите пароль заново!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox2.Text = "";
+                        textBox3.Text = "";
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Вход
+        {
+            if (b == 0)
+            {
+                c1 = 0;
+                c2 = 0;
+                c3 = 0;
+                a = 0;
+                x = 1;
+                textBox1.Visible = true;
+                textBox1.Text = "Введите логин";
+                textBox1.ForeColor = Color.Gray;
+
+                textBox2.Visible = true;
+                textBox2.Text = "Введите пароль";
+                textBox2.ForeColor = Color.Gray;
+
+                textBox3.Visible = false;
+                textBox3.Text = "Повторите пароль";
+                textBox3.ForeColor = Color.Gray;
+
+                checkBox1.Visible = true;
+                b = 1;
+            }
+            if (b == 1 || b > 0 || b != 0)
+            {
+                if (textBox1.Text != "" && textBox1.Text != "Введите логин" || textBox2.Text != "" && textBox2.Text != "Введите пароль")
+                {
+                    string s1 = textBox1.Text;
+                    string s2 = textBox2.Text;
+                    string q = "SELECT COUNT(*) FROM accounts WHERE (login = \"" + s1 + "\" AND pass = \"" + s2 + "\")";
+                    if (textBox1.Text != textBox2.Text)
+                    {
+                        try
+                        {
+                            dbCon = new OleDbConnection(ConS);
+                            dbCon.Open();
+                            using (dbCon)
+                            {
+                                OleDbCommand com = new OleDbCommand(q, dbCon);
+                                string res = com.ExecuteScalar().ToString();
+                                com.ExecuteNonQuery();
+                                if (res != "0")
+                                {
+                                    MessageBox.Show("Добро пожаловать!.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Неверный Логин/Пароль!.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    textBox1.Text = "";
+                                    textBox2.Text = "";
+                                    return;
+                                }
+                            }
+                            dbCon.Close();
+                            b = 0;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ошибка соединения с БД!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            MessageBox.Show(Convert.ToString(a));
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Логин и пароль не могут совпадать!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //событие чекбокса
+        {
+            if (checkBox1.Checked == true && checkBox1.Visible == true)
+            {
+                textBox2.UseSystemPasswordChar = false;
+            }
+            else 
+            {
+                textBox2.UseSystemPasswordChar = true;
+            }
+        }
+        //-----------------------------------------------------------------------События текстбоксов 1 2 3
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (c1 == 0)
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+                c1 = 1;
+            }
+            if (c1 == 1 || c1>0 || c1!=0)
+            {
+                d1 = textBox1.Text;
+                textBox1.Text = d1;
+            }
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (c2 == 0)
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = Color.Black;
+                c2 = 1;
+            }
+            if (c2 == 1 || c2 > 0 || c2 != 0)
+            {
+                d2 = textBox2.Text;
+                textBox2.Text = d2;
+            }
+            if (x == 1)
+            {
+                textBox2.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (c3 == 0)
+            {
+                textBox3.Text = "";
+                textBox3.ForeColor = Color.Black;
+                c3 = 1;
+            }
+            if (c3 == 1 || c3 > 0 || c3 != 0)
+            {
+                d3 = textBox3.Text;
+                textBox3.Text = d3;
+            }
+        }
+    }
+}
